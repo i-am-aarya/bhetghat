272696138969aed9cc6e8832c0eec102f47ab271
i-am-aarya <aryabhattarai180@gmail.com>
2024-12-13 15:04:55 +0545

    update seed script

--- backend/cmd/seed/seed.go
+++ backend/cmd/seed/seed.go
@@ -5,17 +5,33 @@ import (
 	"fmt"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/crypto/bcrypt"
 
 	db "bhetghat-server/database"
 	"bhetghat-server/models"
+	"bhetghat-server/repository"
 )
 
 var ctx = context.Background()
 
-func MakeUser(firstName, lastName string, isAdmin bool) models.User {
+func CreateUniqueIndexes(coll *mongo.Collection, field ...string) error {
+	for _, f := range field {
+		fieldIndex := mongo.IndexModel{
+			Keys:    bson.M{f: 1},
+			Options: options.Index().SetUnique(true),
+		}
+		_, err := coll.Indexes().CreateOne(ctx, fieldIndex)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func MakeUser(firstName, lastName string, isAdmin bool) *models.User {
 	hashedPass, err := bcrypt.GenerateFromPassword(
 		[]byte(fmt.Sprintf("%s_%s", firstName, lastName)),
 		bcrypt.DefaultCost,
@@ -23,32 +39,54 @@ func MakeUser(firstName, lastName string, isAdmin bool) models.User {
 	if err != nil {
 		log.Fatal("Error creating user for seed")
 	}
-	return models.User{
+	return &models.User{
 		FirstName:      firstName,
 		LastName:       lastName,
-		Email:          fmt.Sprintf("%s%s@bhetghat.com", firstName, lastName),
+		Email:          fmt.Sprintf("%s.%s@bhetghat.com", firstName, lastName),
 		Username:       fmt.Sprintf("%s.%s", firstName, lastName),
 		HashedPassword: string(hashedPass),
 		IsAdmin:        isAdmin,
 	}
 }
 
+// SeedUsers creates Indexes, and inserts one admin and one non-admin user in the database
+func SeedUsers(mongoUserRepo *repository.MongoUserRepo, users []*models.User) error {
+	for _, user := range users {
+		_, err := mongoUserRepo.Insert(ctx, user)
+		if err != nil {
+			// log.Fatal("ERROR INSERTING USER: ", err)
+			return err
+		}
+	}
+	return nil
+}
+
 func main() {
+
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(db.DBURI))
 	if err != nil {
 		log.Fatal("ERROR CONNECTING TO DATABASE: ", err)
 	}
 
-	userColl := client.Database("bhetghat").Collection("users")
+	bhetghatDB := client.Database("bhetghat")
+	usersCollName := "users"
+	usersColl := bhetghatDB.Collection(usersCollName)
+	mongoUserRepo := repository.NewMongoUserRepo(bhetghatDB, usersCollName)
 
-	users := []models.User{
-		MakeUser("hari", "bahadur", false),
-		MakeUser("turi", "kumar", true),
+	err = CreateUniqueIndexes(usersColl, "username", "email")
+	if err != nil {
+		log.Fatal("ERROR CREATING UNIQUE INDEXES: ", err)
 	}
 
-	for _, user := range users {
-		userColl.InsertOne(ctx, user)
+	users := []*models.User{
+		MakeUser("hari", "bahadur", false), // hari bahadur is a non-admin user
+		MakeUser("madan", "bahadur", true), // madan bahadur is an admin
 	}
 
-	fmt.Println("DATABASE SEED SUCCESSFUL")
+	err = SeedUsers(mongoUserRepo, users)
+	if err != nil {
+		log.Fatal("ERROR SEEDING USERS: ", err)
+	}
+
+	log.Println("--- SEED USER SUCCESSFUL")
 }
