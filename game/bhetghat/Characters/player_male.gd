22f9b57738e21171062fc688d7346db471143068
i-am-aarya <aryabhattarai180@gmail.com>
2024-12-14 15:55:08 +0545

    fix diag movement; add player-follow camera

--- game/bhetghat/Characters/player_male.gd
+++ game/bhetghat/Characters/player_male.gd
@@ -18,17 +18,15 @@ func _physics_process(_delta):
 	).normalized()
 	
 	update_animation_parameters(input_direction)
-	
 	#updates velocity
 	velocity = input_direction * move_speed
 	
 	#move and slide function uses velocity of character body to move character on  map
 	move_and_slide()
-	
 	pick_new_state()
 	
 func update_animation_parameters(move_input : Vector2):
-		# Don't change animation parameters if there is no move input
+	# Don't change animation parameters if there is no move input
 	if (move_input != Vector2.ZERO):
 		animation_tree.set('parameters/Walk/blend_position', move_input)
 		animation_tree.set('parameters/Idle/blend_position',move_input)
@@ -36,6 +34,6 @@ func update_animation_parameters(move_input : Vector2):
 	# choose state based on waht is happeing with the player
 func pick_new_state():
 	if(velocity != Vector2.ZERO):
-			state_machine.travel("Walk")
+		state_machine.travel("Walk")
 	else:
-			state_machine.travel("Idle")
+		state_machine.travel("Idle")
