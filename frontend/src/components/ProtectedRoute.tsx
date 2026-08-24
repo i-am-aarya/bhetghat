cd6efb3fa564f175e0538cbcccaf110f316551b1
Arya Bhattarai <aryabhattarai180@gmail.com>
2026-08-24 09:45:24 +0545

    protected route now takes more horizontal space on small displays

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -3,26 +3,26 @@ import { useAuthStore } from "@/stores/authStore";
 import { Navigate, Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 
-const ProtectedRoute = ({fullWidth = false}) => {
+const ProtectedRoute = ({ fullWidth = false }) => {
   const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
   const isLoading = useAuthStore((s) => s.isCheckingSession);
 
   if (isLoading) {
-    return (
-      <LoadingPage/>
-    );
+    return <LoadingPage />;
   }
   if (!isAuthenticated) {
-    console.log("not authenticated")
+    console.log("not authenticated");
     return <Navigate to="/login" replace />;
   }
 
-  return <div className="w-screen h-screen">
-    { !fullWidth && <NavBar/> }
-    <main className={fullWidth ? "" : "w-3/4 mx-auto mt-14"}>
-    <Outlet/>
-    </main >
-  </div>
+  return (
+    <div className="w-screen h-screen">
+      {!fullWidth && <NavBar />}
+      <main className={fullWidth ? "" : "md:w-3/4 w-11/12 mx-auto mt-14 pb-4"}>
+        <Outlet />
+      </main>
+    </div>
+  );
 };
 
 export default ProtectedRoute;
